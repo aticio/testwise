b703e54ec5d8aad79f4c6386ae915d2709c4c195
Özgür Atıcı <100781@isbank.com.tr>
2020-04-24 14:42:16 +0300

    workflow definitions

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md","r") as fh:
 
 setup(
     name="testwise",
-    version="0.0.3",
+    version="0.0.4",
     description="A backtester (backtest helper) for testing my trading strategies.",
     packages=find_packages(),
     classifiers=[
