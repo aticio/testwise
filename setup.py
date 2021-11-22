b74123ad5b885ceba2b0f6864911222aefad7a61
aticio <aticiozgur@gmail.com>
2021-11-22 21:55:14 +0300

    version update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="testwise",
-    version="0.0.54",
+    version="0.0.55",
     description="A backtester (backtest helper) for testing my trading strategies.",
     py_modules=["testwise"],
     classifiers=[
